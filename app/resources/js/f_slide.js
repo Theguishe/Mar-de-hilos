a1f459655560ea11a4f3e69e08008c6aa14c84d3
Theguishe <guillermogacc0526@gmail.com>
2023-02-19 00:36:14 -0600

    client page finished - wthut full responsive

--- app/resources/js/f_slide.js
+++ app/resources/js/f_slide.js
@@ -2,3 +2,30 @@ var splide = new Splide( '.splide', {
     type : 'loop',
 });
 splide.mount();
+
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  var swiper = new Swiper(".carousel-swiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
