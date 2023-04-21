8b9c78037591bd290c3e2f64edde1d5543351db1
DiegoJorge <82895581+davidghjg@users.noreply.github.com>
2023-04-21 10:48:42 -0600

    Buton productos
    Actualizacion de los botones y primera prueba

--- app/views/private/dashboard/src/scenes/products/index.jsx
+++ app/views/private/dashboard/src/scenes/products/index.jsx
@@ -1,10 +1,18 @@
+import React from "react";
 import { Box, Typography, useTheme } from "@mui/material";
 import Header from "../../components/header";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../themes";
 import { mockDataInvoices } from "../../data/mockData";
+import Button from "@mui/material/Button";
+
+
 
 const Products = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const columns = [
@@ -71,13 +79,35 @@ const Products = () => {
           },
           "& .MuiCheckbox-root": {
             color: `${colors.greenAccent[200]} !important`,
+          }, 
+          "& .MuiDataGrid-toolbarContainer": {
+            float: "right",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
           },
         }}
       >
-        <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
+        <Button
+          onClick={handleOpen}
+          sx={{
+            background: `${colors.blueAccent[700]}`,
+            color: "#fff",
+            fontSize: "16px",
+            padding: "5px 30px 5px 30px",
+            textTransform: "capitalize",
+            "&:hover": {
+              background: `${colors.blueAccent[800]}`, // Here continues
+            },
+          }}
+        >
+          Add client
+        </Button>
+
+        <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} components={{ Toolbar: GridToolbar }} />
       </Box>
     </Box>
-  );  
+  );
 };
 
 
