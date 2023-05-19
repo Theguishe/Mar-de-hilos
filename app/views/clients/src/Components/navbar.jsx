d58d303ab674a50f9e3e0473088ea136e0b9f1e0
davidghjg <82895581+davidghjg@users.noreply.github.com>
2023-05-19 10:04:10 -0600

    Avances nav bar

--- app/views/clients/src/Components/navbar.jsx
+++ app/views/clients/src/Components/navbar.jsx
@@ -1,30 +1,69 @@
 import { useRef } from "react";
-import { FaBars, FaShopify, FaTimes } from "react-icons/fa";
+//import { FaBars, FaShopify, FaTimes } from "react-icons/fa";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping, faUser, faHeart } from "@fortawesome/free-solid-svg-icons"
-import "../Styles/main.css";
-
+import { faCartShopping, faUser, faHeart, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import "../index.css";
 import React, { useState } from 'react';
 
-function SearchBar() {
+
+function BarraDeBusqueda() {
 	const [Buscar, setBuscar] = useState('');
 
-	const handleSearchChange = (event) => {
-		setBuscar(event.target.value);
+	const handleBuscarChange = (event) => {
+		event.preventDefault();
+		console.log(Buscar);
+		/*function BarraDeBusqueda() {
+	const [Buscar, setBuscar] = useState('');
+
+	const handleBuscarChange = (event) => {
+	}; 
+*/
 	};
 
 	return (
+		/*
 		<input
 			type="text"
 			placeholder="Search"
 			value={Buscar}
-			onChange={handleSearchChange}
+			onChange={handleBuscarChange}
 		/>
-	);
 }
+*/
+		<form className="flex" onSubmit={handleBuscarChange}>
+			<input
+				className="border rounded-l px-2 py-1"
+				type="text"
+				placeholder="Buscar..."
+				value={Buscar}
+				onChange={(event) => setBuscar(event.target.value)}
+			/>
+			<button className="border rounded-r px-2 py-1 bg-blue-500 text-white" type="submit">
+				<FontAwesomeIcon icon={faMagnifyingGlass} size="xl" />
+			</button>
+		</form>
+	);
+};
+
+const Navbar = () => {
+	return (
+		<nav className="flex justify-between items-center p-4">
+
+			<BarraDeBusqueda />
+			<ul className="flex">
+				<li ><a className="mr-5" href="/Usuario"><FontAwesomeIcon icon={faUser} size="xl" /></a></li>
+				<li ><a className="mr-5" href="/Carrito" ><FontAwesomeIcon icon={faCartShopping} size="xl" /></a></li>
+				<li><a className="mr-8" href="/Favoritos"><FontAwesomeIcon icon={faHeart} size="xl"  /></a></li>
+			</ul>
+
+		</nav>
+	);
+};
+
+
+/*
 function Navbar() {
 	const navRef = useRef();
-	const [Buscar, setBuscar] = useState("");
 
 	const showNavbar = () => {
 		navRef.current.classList.toggle(
@@ -35,14 +74,12 @@ function Navbar() {
 	return (
 		<header>
 			<h3>LOGO</h3>
-			<nav ref={navRef}>
+			<nav className="flex justify-between items-center p-4">
 				<button>
-
+				<BarraDeBusqueda />
 				</button>
-				<a href="/#"><FontAwesomeIcon icon={faUser} size="xl" style={{ color: "#ffffff", }} /></a>
-				<a href="/#"><FontAwesomeIcon icon={faCartShopping} size="xl" style={{ color: "#ffffff", }} /></a>
-				<a href="/#"><FontAwesomeIcon icon={faHeart} size="xl" style={{ color: "#ffffff", }} /></a>
-				<a href="/#">About me</a>
+				<a className="mr-4" href="/#"><FontAwesomeIcon icon={faHeart} size="xl" style={{ color: "#ffffff", }} /></a>
+				<a className="mr-4" href="/#">About me</a>
 				<button
 					className="nav-btn nav-close-btn"
 					onClick={showNavbar}>
@@ -56,5 +93,6 @@ function Navbar() {
 		</header>
 	);
 }
+*/
 
 export default Navbar;
