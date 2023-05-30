4e0479c8a4ca049b10ac026ff82bf043569f6ccd
Theguishe <guillermogacc0526@gmail.com>
2023-05-29 21:38:39 -0600

    footer component finished
    starting populating product cards

--- app/views/clients/src/Components/footer.jsx
+++ app/views/clients/src/Components/footer.jsx
@@ -1,34 +1,104 @@
 import Box from "@mui/material/Box";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 const Footer = () => {
   return (
-    <Box className=''>
-      <Box className="">
-        <Box className="flex flex-col items-center justify-center max-w-screen m-auto">
-          <Box>
-            <img src="logo" alt="logo-mardehilos" />
-          </Box>
-          <nav>
-            <ul className="m-0 p-0 flex justify-center list-none">
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Services</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
-            </ul>
-          </nav>
-        </Box>
-        <Box>
-          <p>&copy; 2023 Mar de hilos. All rights reserved.</p>
-        </Box>
-      </Box>
+    // <Box className='bg-gray-100'>
+    //   <Box className="">
+    //     <Box className="flex flex-col items-center justify-center max-w-screen m-auto">
+    //       <Box>
+    //         <img src="logo" alt="logo-mardehilos" />
+    //       </Box>
+    //       <nav>
+    //         <ul className="m-0 p-0 flex justify-center list-none">
+    //           <li>
+    //             <a href="#">Home</a>
+    //           </li>
+    //           <li>
+    //             <a href="#">About</a>
+    //           </li>
+    //           <li>
+    //             <a href="#">Services</a>
+    //           </li>
+    //           <li>
+    //             <a href="#">Contact</a>
+    //           </li>
+    //         </ul>
+    //       </nav>
+    //     </Box>
+    //     <Box>
+    //       <p>&copy; 2023 Mar de hilos. All rights reserved.</p>
+    //     </Box>
+    //   </Box>
+    // </Box>
+    <Box>
+      <footer className="bg-gray-100 text-black py-10">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-wrap items-center justify-around">
+            <div className="w-full lg:w-1/4 mb-8 lg:mb-0">
+              <h2 className="text-2xl font-bold mb-4 text-center">
+                Mar de hilos
+              </h2>
+              <ul className="text-center">
+                <li className="mb-2">
+                  <a href="/">Home</a>
+                </li>
+                <li className="mb-2">
+                  <a href="/">About Us</a>
+                </li>
+                <li className="mb-2">
+                  <a href="/">Careers</a>
+                </li>
+                <li className="mb-2">
+                  <a href="/">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+            <div className="w-full lg:w-1/4 mb-8 lg:mb-0">
+              <h2 className="text-2xl font-bold mb-4 text-center">Products</h2>
+              <ul className="text-center">
+                <li className="mb-2">
+                  <a href="/">Recomendados</a>
+                </li>
+                <li className="mb-2">
+                  <a href="/">Carrito</a>
+                </li>
+                <li className="mb-2">
+                  <a href="/">Favoritos</a>
+                </li>
+              </ul>
+            </div>
+            <div className="w-full lg:w-1/4">
+              <h2 className="text-2xl font-bold mb-4">Follow Us</h2>
+              <ul className="flex">
+                <li className="mr-4">
+                  <a href="/" target="_blank" rel="noopener noreferrer">
+                    <WhatsAppIcon />
+                  </a>
+                </li>
+                <li className="mr-4">
+                  <a href="/" target="_blank" rel="noopener noreferrer">
+                    <InstagramIcon />
+                  </a>
+                </li>
+                <li className="mr-4">
+                  <a href="/" target="_blank" rel="noopener noreferrer">
+                    <FacebookIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="mt-8 text-center">
+            <p>
+              &copy; {new Date().getFullYear()} Mar de Hilos. All rights
+              reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
     </Box>
   );
 };
